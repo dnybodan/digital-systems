5240a4ae6dc43a7097fd783dbf58bbe5860e281d
Dan Nybo <dnybo.dan@gmail.com>
2023-12-21 14:27:29 -0700

    initial commit

--- spi/seven_segment.vhd
+++ spi/seven_segment.vhd
@@ -1,1 +1,98 @@
-../vhdl/seven_segment.vhd
+-- /***************************************************************************
+-- *
+-- * Module: seven_segment
+-- *
+-- * Author: Daniel Nybo
+-- * Class: ECEN 620
+-- * Date: September 27, 2023
+-- *
+-- * Description: this is the seven segment display module for the Nexys DDR
+-- *              board. It takes in a 32 bit vector and displays it on the
+-- *              seven segment display.
+-- *
+-- ****************************************************************************/
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity seven_segment is
+    Generic(
+        CLK_FREQ: integer := 100000000; -- Default for Nexys DDR board
+        MIN_DIGIT_DISPLAY_TIME_MS: integer := 2;
+        MS_PER_SEC: integer := 1000
+    );
+    Port(
+        clk: in std_logic;
+        data: in std_logic_vector(31 downto 0); -- 8 x 4 bits for each seven-segment digit
+        anode: out std_logic_vector(7 downto 0);
+        segment: out std_logic_vector(6 downto 0)
+    );
+end seven_segment;
+
+architecture Behavioral of seven_segment is
+    signal counter: integer := 0;
+    signal display_count: integer := (CLK_FREQ * MIN_DIGIT_DISPLAY_TIME_MS) / MS_PER_SEC;
+    signal current_digit: integer := 0; -- To select which digit to display
+    signal segment_data: std_logic_vector(3 downto 0);
+    signal rst: std_logic := '0';
+    -- Segment decoder
+    function get_segment_pattern(digit: std_logic_vector(3 downto 0)) return std_logic_vector is
+    begin
+        case digit is
+            when "0000" => return "1000000"; -- 0
+            when "0001" => return "1111001"; -- 1
+            when "0010" => return "0100100"; -- 2
+            when "0011" => return "0110000"; -- 3
+            when "0100" => return "0011001"; -- 4
+            when "0101" => return "0010010"; -- 5
+            when "0110" => return "0000010"; -- 6
+            when "0111" => return "1111000"; -- 7
+            when "1000" => return "0000000"; -- 8
+            when "1001" => return "0010000"; -- 9
+            when "1010" => return "0001000"; -- A
+            when "1011" => return "0000000"; -- B
+            when "1100" => return "1000110"; -- C
+            when "1101" => return "1000000"; -- D
+            when "1110" => return "0000110"; -- E
+            when "1111" => return "0001110"; -- F
+            when others => return "1000000"; -- Default case
+        end case;
+    end function;
+
+begin
+    -- Process for multiplexing the display
+    process(clk, rst)
+    begin
+        if rst = '1' then
+            counter <= 0;
+            current_digit <= 0;
+        elsif rising_edge(clk) then
+            counter <= counter + 1;
+            if counter = display_count then
+                counter <= 0;
+                if current_digit = 7 then
+                    current_digit <= 0;
+                else
+                    current_digit <= current_digit + 1;
+                end if;
+            end if;
+        end if;
+    end process;
+
+    -- selecting the segment data
+    segment_data <= data((4 * current_digit + 3) downto (4 * current_digit));
+    segment <= get_segment_pattern(segment_data);
+    -- select which anode is on
+    anode <= "11111110" when current_digit = 0 else -- MSB digit on
+             "11111101" when current_digit = 1 else -- second digit on
+             "11111011" when current_digit = 2 else -- third digit on
+             "11110111" when current_digit = 3 else -- fourth digit on
+             "11101111" when current_digit = 4 else -- fifth digit on
+             "11011111" when current_digit = 5 else -- sixth digit on
+             "10111111" when current_digit = 6 else -- seventh digit on
+             "01111111" when current_digit = 7 else -- LSB digit on
+             "11111111";                            -- all off
+             
+        
+end Behavioral;
--- vhdl/tx.sv
+++ vhdl/tx.sv
@@ -1,1 +1,197 @@
-../uart_receiver/tx.sv
+/***************************************************************************
+*
+* Module: tx
+*
+* Author: Daniel Nybo
+* Class: ECEN 620
+* Date: September 13, 2023
+*
+* Description: this is a parameterized asynchronous transmitter which takes 
+* an 8 bit data value and outputs the serial out signal. The serial out 
+* signal is a 1 start bit, 8 data bits, 1 parity bit, and 1 stop bit.
+* The parameters for this module are the clock frequency, the baud rate,
+* and the parity type. The parity type is either odd or even parity.
+*
+****************************************************************************/
+`default_nettype none
+`timescale 1ns / 1ps
+
+module tx #(parameter CLK_FREQ=100000000, parameter BAUD_RATE=19200, parameter PARITY_MODE=1) (
+    input wire logic clk, rst, send,
+    input wire logic[7:0] din,
+    output logic busy, tx_out);
+    
+    // state type for transmitter FSM
+    typedef enum logic[2:0] {idle, start, bits, par, stop, ack, ERR='X} stateType;
+    stateType ns, cs;
+    
+    // logic signals for data path section of transmitter
+    logic startBit, dataBit, parityBit, ackBit, busy_r;
+
+    // bit counter signals
+    logic[2:0] bitNum;
+    logic incBit, clrBit, bitDone;
+   
+    // logic signal for baud rate timer
+    logic timerDone, clrTimer;
+    logic[12:0] baudTimer;
+ 
+    // constants
+    localparam BAUD_TIMER_MAX = (CLK_FREQ/BAUD_RATE);
+
+    localparam BIT_CTR_MAX = 3'd7;
+
+    // Busy report
+    assign busy = busy_r;
+    // generate busy report
+    always_ff @(posedge clk)
+        if (rst)
+            busy_r <= 0;
+        else
+            if (startBit)
+                busy_r <= 1;
+            else if (ackBit)
+                busy_r <= 0;
+    
+    //transmiter datapath code
+    always_ff @(posedge clk)
+        if(startBit)
+            tx_out <= 0;
+        else if(dataBit)
+            tx_out <= din[bitNum];
+        else if(parityBit)
+            if(!PARITY_MODE)
+                tx_out <= ^din; //parity calculation for even parity
+            else
+                tx_out <= ~^din; //parity calculation for odd parity
+        else 
+            tx_out <= 1; //idle
+     
+    // Baud Rate Timer
+    assign timerDone = (baudTimer == BAUD_TIMER_MAX)?1:0;
+    // Timer update logic
+    always_ff @(posedge clk)
+        // reset clause
+        if (rst)
+            baudTimer <= 0;
+        else
+            if (clrTimer || timerDone)
+                baudTimer <= 0;
+            else
+                baudTimer <= baudTimer + 1; 
+              
+    // Bit Counter
+    assign bitDone = (bitNum == BIT_CTR_MAX)?1:0;
+    // Counter update logic
+    always_ff @(posedge clk)
+        // reset clause
+        if (rst)
+            bitNum <= 0;
+        else
+            if (clrBit)
+                bitNum <= 0;
+            else if (incBit)
+                bitNum <= bitNum + 1;
+
+    // transmitter FSM 
+    always_comb
+        begin 
+        ns = ERR;
+        clrTimer = 0;
+        startBit = 0;
+        incBit =0;
+        clrBit = 0;
+        dataBit = 0;
+        parityBit = 0;
+        ackBit = 0;
+        // reset clause
+        if (rst)
+        begin
+            ns = idle;
+            clrTimer = 0;
+            startBit = 0;
+            incBit =0;
+            clrBit = 0;
+            dataBit = 0;
+            parityBit = 0;
+            ackBit = 0;
+        end
+        else
+            case (cs)
+                // idle state is the default state
+                // the transmitter will stay in this state
+                // until the send signal is asserted
+                idle:
+                begin
+                    clrTimer = 1;
+                    if (send)
+                        ns = start;
+                    else 
+                        ns = cs;
+                end
+                // start state is the first state of the transmitter
+                // it asserts the start bit and sets the bit counter to 0
+                // it also starts the baud rate timer
+                start:
+                begin
+                    startBit = 1;
+                    if (timerDone)
+                    begin
+                        clrBit = 1;
+                        ns = bits;
+                    end
+                    else
+                        ns = cs;
+                end
+                // bits state is the state where the data bits are sent
+                // it asserts the data bit and increments the bit counter
+                // it starts the bit counter and check baud and bit counters
+                bits:
+                begin
+                    dataBit = 1;
+                    if (timerDone && ~bitDone)
+                    begin
+                        incBit = 1;
+                        ns = cs;
+                    end
+                    else if (~timerDone)
+                        ns = cs;
+                    else 
+                        ns = par;
+                end
+                // the parity state is where the parity bit is sent     
+                par:
+                begin
+                    parityBit = 1;
+                    if (~timerDone)
+                        ns = cs;
+                    else 
+                        ns = stop;
+                end
+                // the stop state is where the stop bit is sent
+                stop:
+                begin
+                    if (~timerDone)
+                        ns = cs;
+                    else
+                        ns = ack;
+                end
+                // the ack state is where the busy signal is deasserted
+                // and the next state is set to idle
+                ack:
+                begin
+                    ackBit = 1;
+                    if(send)
+                        ns = cs;
+                    else
+                        ns = idle;
+                end  
+            endcase
+    end      
+     
+    //state register for transmitter FSM
+    always_ff @(posedge clk)
+        cs <= ns;       
+    
+    
+endmodule
